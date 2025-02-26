cd6ee24f438b1745ce8390efbe46f4e2d5be054b
theo <theobfernandez@gmail.com>
2025-02-26 11:40:37 -0800

    html parsing demo

--- src/components/home/EventCard.tsx
+++ src/components/home/EventCard.tsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { Event } from "react-big-calendar";
 import Link from "next/link";
+import parse, {
+  domToReact,
+  HTMLReactParserOptions,
+  Element,
+} from "html-react-parser";
 
 interface EventWithDescription extends Event {
   description: string;
@@ -15,7 +20,6 @@ const extractLink = (description: string | undefined): string | null => {
   if (!description) {
     return null;
   }
-
   const regex = /https:\/\/[^\s"]+/;
   const match = description.match(regex);
   return match ? match[0] : null;
@@ -24,7 +28,6 @@ const extractLink = (description: string | undefined): string | null => {
 const EventCard = ({ event }: EventCardProps) => {
   const [linkOrNot, setLinkOrNot] = useState<string | null>(null);
   console.log("event", event);
-
   console.log("desc", extractLink(event.description?.toLocaleString()));
 
   useEffect(() => {
@@ -32,15 +35,44 @@ const EventCard = ({ event }: EventCardProps) => {
     setLinkOrNot(link);
   }, [event.description]);
 
+  // Options for html-react-parser to style links blue
+  const parserOptions: HTMLReactParserOptions = {
+    replace: (domNode) => {
+      // Check if the node is an element and is an anchor tag
+      if (
+        domNode instanceof Element &&
+        domNode.name === "a" &&
+        domNode.attribs.href
+      ) {
+        return (
+          <a
+            href={domNode.attribs.href}
+            style={{ color: "blue" }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {domToReact(domNode.children, parserOptions)}
+          </a>
+        );
+      }
+    },
+  };
+
   return (
     <div className="z-10 flex-col rounded-2xl border border-gray-200 bg-white p-5 shadow-lg">
       <div className="text-gray-500">{event.start?.toLocaleDateString()}</div>
       <div className="p-2" />
-      <div className="text-rao-black">{event.title?.toLocaleString()}</div>
+      {event.title === "Event" ? (
+        <div className="text-rao-black">
+          {parse(event.description?.toLocaleString() || "", parserOptions)}
+        </div>
+      ) : (
+        <div className="text-rao-black">{event.title?.toLocaleString()}</div>
+      )}
       <div className="p-1" />
       <div className="w-full border border-rao-yellow"></div>
       <div className="p-2" />
-      {linkOrNot && (
+      {linkOrNot && event.title !== "Event" && (
         <Link href={linkOrNot}>
           <div className="flex items-center justify-end gap-1">
             <div className="font-bold text-rao-blue">read more</div>
